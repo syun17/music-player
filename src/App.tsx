6ed8e64dce0cfe955bf816208d63ebbc23bb9fed
syun17 <shunki.s777@gmail.com>
2025-07-24 12:11:50 +0900

    Shadcnの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Button
-        </button>
+        <Button>Click me</Button>
       </div>
     </>
   );
