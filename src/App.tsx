397654ce6fdfa1520e5231a010958e6065f6292f
syun17 <shunki.s777@gmail.com>
2025-07-24 12:03:04 +0900

    環境構築TailWindCSS導入

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
